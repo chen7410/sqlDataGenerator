818dd31740d74629724e17b1c29bd375a146ea86
Minqing <chen7410@uw.edu>
2019-07-05 16:13:32 -0700

    add get random date

--- src/outputFile.scala
+++ src/outputFile.scala
@@ -1,16 +1,23 @@
 import java.io.File
 import java.io.PrintWriter
+import java.time.LocalDate
 import scala.io.Source
 import scala.util.Random
 
 object FileProcessing {
     val COUNTRY_PATH = "\\scalaworkspace\\DataGenerator\\country.csv"
     val CAR_MAKER = "\\scalaworkspace\\DataGenerator\\car_maker.csv"
+    val DATE = "\\scalaworkspace\\DataGenerator\\date.txt"
+    val OUTPUT_FILE = "table.txt"
     val NUM_RECORD = 100
     val RANDOM = new Random()
+//    val DateTimeFormatter = new DateTimeFormatter("YYY-MM-DD")
+
+    var startDate =  LocalDate.now()
+    var endDate = LocalDate.now()
+
     var country = List[String]()
     var carMaker = List[String]()
-    var aa = List[String]()
 
     def main(args: Array[String]) {
         country = readFromSourceAsList(COUNTRY_PATH)
@@ -21,23 +28,34 @@ object FileProcessing {
 //        println(getRandomFromList(carMaker))
 //        println(getRandomFromList(carMaker))
 
-        createTable(NUM_RECORD)
+        setStartDateEndDate(DATE)
+        outputFile(NUM_RECORD, OUTPUT_FILE)
+
     }
 
-    def createTable(numRecord: Int): Unit ={
-        val sb = new StringBuilder
-        for (row <- 1 to NUM_RECORD) {
-            sb ++= row.toString
-            sb += ','
-            sb ++= getRandomFromList(country)
-            sb += ','
-            sb ++= getRandomFromList(carMaker)
-            sb ++= System.lineSeparator()
-        }
-        val writer = new PrintWriter(new File("table.txt"))
-        writer.write(sb.toString())
-        writer.close()
+    /**
+      * File format example
+      * start date:YYYY-MM-DD
+      * @param path
+      */
+    def setStartDateEndDate(path: String): Unit = {
+        val lines: List[String] = Source.fromFile(path).getLines.toList
+        startDate = LocalDate.parse(lines(0).split(":")(1))
+        endDate = LocalDate.parse(lines(1).split(":")(1))
+        println(startDate.toEpochDay)
     }
+
+    /**
+      *  call after setStartDateEndDate
+      * @param start
+      * @param end
+      * @return a random between start date and end date such as 2017-09-09
+      */
+    def getRandomDateBetween(start: LocalDate, end: LocalDate): LocalDate = {
+        LocalDate.ofEpochDay(RANDOM.between(start.toEpochDay, end.toEpochDay + 1))
+    }
+
+
     /**
       * The given list size must > 0
       * @param list
@@ -55,9 +73,31 @@ object FileProcessing {
       */
     def readFromSourceAsList(path: String): List[String] = {
         val bufferedSource = Source.fromFile(path)
-        val lines = bufferedSource.getLines().toList
+        val list = bufferedSource.getLines().toList
         bufferedSource.close
-        lines
+        list
     }
 
+    /**
+      * Override if needed, the output file as following
+      * format 1,country1,carmaker1
+      *        2,country2,carmaker2
+      * @param numRecord the number of record generated
+      */
+    def outputFile(numRecord: Int, pathName: String): Unit ={
+        val sb = new StringBuilder
+        for (row <- 1 to NUM_RECORD) {
+            sb ++= row.toString
+            sb += ','
+            sb ++= getRandomFromList(country)
+            sb += ','
+            sb ++= getRandomFromList(carMaker)
+            sb += ','
+            sb ++= getRandomDateBetween(startDate, endDate).toString
+            sb ++= System.lineSeparator()
+        }
+        val writer = new PrintWriter(new File(pathName))
+        writer.write(sb.toString())
+        writer.close()
+    }
 }
