c41c21654dd1d5dc5a4ce13545dd262d102eedf8
Minqing <chen7410@uw.edu>
2019-07-05 13:45:00 -0700

    create table based on the given country and car maker

--- src/outputFile.scala
+++ src/outputFile.scala
@@ -1,28 +1,63 @@
+import java.io.File
+import java.io.PrintWriter
 import scala.io.Source
-import collection.immutable.Set
 import scala.util.Random
+
 object FileProcessing {
-	var country = List[String]()
-	var carMaker = Set[String]()
-	var count = 1
-	val random = new Random()
-	def main(args: Array[String]) {
-		val fileName = "D:\\scalaworkspace\\DataGenerator\\country.csv"
-		country = readFromSourceAsList(fileName, fileName)
-//		country.foreach(println)
-		println(country(random.nextInt(country.length - 1)))
-		println(country(random.nextInt(country.length - 1)))
-	}
-	
-	/*
-	read the file data from the file path to the given set
-	*/
-	def readFromSourceAsList(fileName: String, path: String): List[String] = {
-		val bufferedSource = Source.fromFile(fileName)
-		val lines = bufferedSource.getLines().toList
-//		lines.foreach(println)
-//		 println(country.size)
-		bufferedSource.close
-		lines
-	}
+    val COUNTRY_PATH = "\\scalaworkspace\\DataGenerator\\country.csv"
+    val CAR_MAKER = "\\scalaworkspace\\DataGenerator\\car_maker.csv"
+    val NUM_RECORD = 100
+    val RANDOM = new Random()
+    var country = List[String]()
+    var carMaker = List[String]()
+    var aa = List[String]()
+
+    def main(args: Array[String]) {
+        country = readFromSourceAsList(COUNTRY_PATH)
+//        println(getRandomFromList(country))
+//        println(getRandomFromList(country))
+
+        carMaker = readFromSourceAsList(CAR_MAKER)
+//        println(getRandomFromList(carMaker))
+//        println(getRandomFromList(carMaker))
+
+        createTable(NUM_RECORD)
+    }
+
+    def createTable(numRecord: Int): Unit ={
+        val sb = new StringBuilder
+        for (row <- 1 to NUM_RECORD) {
+            sb ++= row.toString
+            sb += ','
+            sb ++= getRandomFromList(country)
+            sb += ','
+            sb ++= getRandomFromList(carMaker)
+            sb ++= System.lineSeparator()
+        }
+        val writer = new PrintWriter(new File("table.txt"))
+        writer.write(sb.toString())
+        writer.close()
+    }
+    /**
+      * The given list size must > 0
+      * @param list
+      * @return a random element from the given list
+      */
+    def getRandomFromList(list: List[String]): String = {
+        list(RANDOM.nextInt(list.length))
+    }
+
+    /**
+      * There must be a parent folder for DataGenetor,
+      * for example : "\\parentfolder\\DataGenerator\\country.csv"
+      * @param path
+      * @return a list of data
+      */
+    def readFromSourceAsList(path: String): List[String] = {
+        val bufferedSource = Source.fromFile(path)
+        val lines = bufferedSource.getLines().toList
+        bufferedSource.close
+        lines
+    }
+
 }
